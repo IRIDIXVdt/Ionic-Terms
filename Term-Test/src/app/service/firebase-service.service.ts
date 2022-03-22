4fe9bd551548fcbd961df6c1bdcd23b4d88a8b2d
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-03-22 21:56:36 +0800

    data Adding Service Implemented

--- Term-Test/src/app/service/firebase-service.service.ts
+++ Term-Test/src/app/service/firebase-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { collection, setDoc, doc, getDocs, getFirestore, getDocFromServer } from "firebase/firestore";
+const db = getFirestore();
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseServiceService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  addDataService(collection, data) {
+    return this.db.collection(collection).add(data);
+  }
 }
