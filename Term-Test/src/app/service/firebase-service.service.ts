a5d0ee55d3a6c01201c7f5336e55bb221296b6a4
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-03-23 22:28:25 +0800

    firebase demo data reading

--- Term-Test/src/app/service/firebase-service.service.ts
+++ Term-Test/src/app/service/firebase-service.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { collection, setDoc, doc, getDocs, getFirestore, getDocFromServer } from "firebase/firestore";
-const db = getFirestore();
-
+// import { collection, setDoc, doc, getDocs, getFirestore, getDocFromServer } from "firebase/firestore";
+// const db = getFirestore();
 @Injectable({
   providedIn: 'root'
 })
@@ -13,4 +12,9 @@ export class FirebaseServiceService {
   addDataService(collection, data) {
     return this.db.collection(collection).add(data);
   }
+
+  getDataService() {
+    return this.db.collection("TestCollection1").snapshotChanges();
+  }
+  
 }
--- Term-Test/src/app/tab1/tab1.page.ts
+++ Term-Test/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseServiceService } from '../service/firebase-service.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,8 +8,46 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() { }
+  loadedData: any;
+
+  constructor(private fs: FirebaseServiceService) {
+    this.loadTestData();
+  }
+  loadTestData() {
+    const dataloadingSubrscription = this.fs.getDataService().subscribe((res) => {
+      this.loadedData = res.map(
+        e => {
+          return {
+            id: e.payload.doc.id,
+            age: e.payload.doc.data()['age'],
+            gender: e.payload.doc.data()['gender'],
+          }
+        })
+        console.log('e loaded!', this.loadedData);
+        dataloadingSubrscription.unsubscribe();
+    })
+  }
+
   testClick() {
     console.log("test button clicked");
+    const data = {
+      first: "name",
+      star: 2,
+      testThird: "third"
+    }
+    this.fs.addDataService("NewCollection", data);
   }
 }
+
+
+
+
+
+
+
+
+
+
+// 
+
+
