bcddaadac4f39c74407e1f2790071175887fb3ff
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-04-09 23:13:50 +0800

    result Array List Available

--- Term Selection/displayNew.py
+++ Term Selection/displayNew.py
@@ -7,9 +7,30 @@ with open('Term Selection/powerPreProcessedText.txt') as f:
 
 print(len(lines))
 
+# lines are now all in lines
+
 term = 0
+
+termItem = ''
+resultArray = []
+
 for lineItem in lines:
+
     if ''': (''' in lineItem:
         term += 1
+        resultArray.append(termItem)
+        termItem = lineItem
+    else:
+        termItem += lineItem
+        
+resultArray.append(termItem)
+resultArray.pop(0)
 
 print(term)
+# now we confirmed how many terms there are
+
+print(len(resultArray))
+print(resultArray[0])
+
+
+
